f8d3307ad7033221275523f5c90da0c63ec39995
Tommaso M <84441861+Traster-uni@users.noreply.github.com>
2024-03-03 12:40:24 +0100

    Added python script for plotting graphs of data, still incomplete.

--- python-scripts/dataplotter.py
+++ python-scripts/dataplotter.py
@@ -1,8 +1,35 @@
 import matplotlib.pyplot as pypl
+import random as rdm
+import os 
 
 
-def plot_insulin(d: str):
+def plot_glucose(d: str):
     #EXPECTED .csv formatted insulin_value, time
+    g = []
+    t = []
     with open(d, "r") as f:
-        for line in f:
-            f.readline()
+        for i in f:
+            line = f.readline().rstrip("\n")
+            line = line.split(",")
+            print(line)
+            g += [int(line[0])] #should be float
+            t += [int(line[1])]
+
+    pypl.plot(g,t)
+    pypl.xlabel("t(s)")
+    pypl.ylabel("glucose(mg/dL)")
+    pypl.title("glucose evolution")
+    pypl.show()
+
+    return
+
+
+if __name__ == '__main__':
+
+    with open(os.getcwd()+"/demofile.txt", "w") as wr:
+        for i in range(100):
+            s = "" + str(rdm.randint(1, 10)) + "," + str(rdm.randint(11,20)) + "\n"
+            wr.write(s)
+
+    plot_glucose(os.getcwd()+"/demofile.txt")
+
