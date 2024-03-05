e6ea19f69bd281f9a7e01e8383291af443f26916
d-marretta <danielemarretta@gmail.com>
2024-03-05 18:26:32 +0100

    Added comments to check time function

--- models/time/src/my_time.cpp
+++ models/time/src/my_time.cpp
@@ -9,10 +9,14 @@ double get_time(struct time* curr_time){
 }
 
 bool check_time(struct time* curr_time, int min, int target){
-    double epsilon = pow(10,-5);
-    // eliminate double imprecision
+    double epsilon = pow(10,-6);
+    // eliminate double imprecision: if the curr minutes is a double like 5.99999999999998 take the rounded one
+    // otherwise take the original
     double minutes = (abs(round(curr_time -> minutes) - curr_time -> minutes) <= epsilon) ? round(curr_time -> minutes) : curr_time -> minutes;
     double modulo = fmod(minutes, min);
+
+    // check if modulo is small enough; example: if target is 0 and modulo is 0.0000000000000098 due to double imprecision,
+    // return true still
     return modulo <= (epsilon + target);
 }
 
