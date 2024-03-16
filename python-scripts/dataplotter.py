a515ad864a09e53d158ce0b563455d8bd3261af1
Tommaso M <84441861+Traster-uni@users.noreply.github.com>
2024-03-16 18:31:02 +0100

    Completed dataplotter.py

--- python-scripts/dataplotter.py
+++ python-scripts/dataplotter.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as pypl
-import random as rdm
+import matplotlib.dates as pldate
+import matplotlib.ticker as ticker
+from datetime import datetime
 import os 
 
 
@@ -7,14 +9,20 @@ def plot_glucose(fname: str):
     # EXPECTED .csv formatted insulin_value,time
     d = []
     t = []
+    cut = 0
     with open(fname, "r") as f:
         for li in f:
             line = f.readline().rstrip("\n")
             if line != "":
                 line = line.split(",")
-                d += [int(line[0])]  # should be float
-                t += [int(line[1])]
+                d += [float(line[0])]  # should be float
+                time_list = line[1].split(":")
+                if time_list[0] != "24":
+                    cut += 1
+                    seconds = [time_list[2][:2]]
+                    t += [datetime.strptime(":".join(tuple(time_list[:2] + seconds)), "%H:%M:%S")]
 
+    d = d[:cut]
     title = "empty"
     y_label = "empty"
     if "glucose" in fname.lower():
@@ -26,10 +34,21 @@ def plot_glucose(fname: str):
         title = "insulin evolution"
         y_label = "insulin(mg/dL)"
 
+    locator = pldate.MinuteLocator(range(0,1440,60))
     pypl.title(title)
     pypl.ylabel(y_label)
-    pypl.plot(d, t)
+    # x label is for time
+    ax = pypl.gca()
+    t_num = pldate.date2num(t)
+
+    fmt = pldate.DateFormatter("%H:%M:%S")
+    ax.xaxis.set_major_formatter(fmt)
+    ax.xaxis.set_major_locator(locator)
     pypl.xlabel("t(s)")
+    pypl.xticks(rotation=25)
+    pypl.grid(True, "major", "y")
+    pypl.plot(t_num, d)
+    ax.set_xlim(left=t_num[0], right=t_num[len(t_num)-1])
     pypl.show()
     return
 
