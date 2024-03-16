898cf318dd51f35802ff55740977c9f77789e26d
d-marretta <danielemarretta@gmail.com>
2024-03-16 16:23:55 +0100

    Plot fix

--- python-scripts/dataplotter.py
+++ python-scripts/dataplotter.py
@@ -35,12 +35,5 @@ def plot_glucose(fname: str):
 
 
 if __name__ == '__main__':
-    l1 = [_ for _ in range(0, 101)]
-    l2 = [_ for _ in range(100, 201)]
 
-    with open(os.getcwd()+"glucose_demo.txt", "w") as wr:
-        for i in range(len(l1)):
-            s = "" + str(l1[i]) + "," + str(l2[i]) + "\n"
-            wr.write(s)
-
-    plot_glucose(os.getcwd()+"/demofile.txt")
+    plot_glucose(os.getcwd()+"/glucose_data.csv")
